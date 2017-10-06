c852891127bca942522cebf0f752b2a9aae3e95c
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-10-06 11:38:39 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+        int n,m, i, j;
+        cout << "enter n";
+        cin >> n;
+        cout << "enter m:";
+        cin >> m;
+        int A[n][m];
+        int B[n][m];
+        cout << "Enter first massive's elements:"<< endl;
+        for (int i=0; i<n;i++);
+        for (int j=0; j<m;j++);
+        cin >> A[i][j];
+        cout << "Enter second massive's elements"
+        for (int i=0; i<n; i++);
+        for (int j=0; j<m; j++);
+        cin >> B[i][j];
+        for (int i=0; i<n;i++);
+        {
+                for (int j=0;j<m; j++);
+                cout << A[i][j] + B[i][j];
+
+        }
+        return 0;
+}
+
