796eec0852753b43b04fe1e761645311cd47d9dd
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-12-21 01:19:51 +0300

    Update summa.cpp

--- summa.cpp
+++ summa.cpp
@@ -10,16 +10,16 @@ int main()
         int A[n][m];
         int B[n][m];
         cout << "Enter first massive's elements:"<< endl;
-        for (int i=0; i<n;i++);
-        for (int j=0; j<m;j++);
+        for (int i=0; i<n;i++)
+        for (int j=0; j<m;j++)
         cin >> A[i][j];
         cout << "Enter second massive's elements"
-        for (int i=0; i<n; i++);
-        for (int j=0; j<m; j++);
+        for (int i=0; i<n; i++)
+        for (int j=0; j<m; j++)
         cin >> B[i][j];
-        for (int i=0; i<n;i++);
+        for (int i=0; i<n;i++)
         {
-                for (int j=0;j<m; j++);
+                for (int j=0;j<m; j++)
                 cout << A[i][j] + B[i][j];
 
         }
