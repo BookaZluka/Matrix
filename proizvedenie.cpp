8b139626b32a9da866ce844faead38e56f5335a6
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-12-21 01:14:21 +0300

    Update proizvedenie.cpp

--- proizvedenie.cpp
+++ proizvedenie.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 
-using namespace std;
 
-int main()
-{
-        int n, m, k ;
-        cout << "Enter massives' size: " << endl;
+
+    using namespace std;
+
+    int main() {
+        int n, m, k;
+        cout << "Введите размер массива" << endl;
         cin >> n >> m;
-        cout << "Enter second massive's column: " << endl;
+        cout << "Введите количество столбцов второго массива" << endl;
         cin >> k;
         int a[n][m], b[m][k], c[n][k];
-        for (int i = 0; i < n; i++){
-                for (int j = 0; j < m; j++){
-                        cin >> a[i][j];
-                }
-        }
-cout << endl;
-        for (int i = 0; i < m; i++){
-                for (int j = 0; j < k; j++){
-                        cin >> b[i][j];
-                }
+        cout<<"Введите элементы первого массива: ";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> a[i][j];
+            }
         }
         cout << endl;
-        for (int i = 0; i<n;i++){
-                for (int j = 0;j<k;j++){
-                        c[i][j] = 0;
-                }
+        cout<<"Введите элементы второго массива размера "<<m<<"x"<<k<<":";
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) {
+                cin >> b[i][j];
+            }
         }
-
-        for (int i = 0; i < n; i++){
-          for (int j = 0; j < k; j++){
-                        for (int r = 0; r < m; r++){
-                                c[i][j] = c[i][j] + a[i][r] *  b[r][j];
-
-                        }
+        cout << endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                c[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                for (int r = 0; r < m; r++) {
+                    c[i][j] = c[i][j] + a[i][r] * b[r][j];
+                }
                 cout << c[i][j] << " ";
-                }
-                cout << endl;
+            }
+            cout << endl;
         }
-}
+    }
